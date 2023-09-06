f253b7556c47a68688c6751d9faaa6727dfde7fa
donnwilzy <ask4godswill@gmail.com>
2023-09-06 01:32:13 +0100

    changepassword

--- controllers/authController.js
+++ controllers/authController.js
@@ -138,7 +138,6 @@ const generateOTPCode = () => {
     try {
       const { username, email, password } = req.body;
   
-      // Check if either username or email is provided
       if (!username && !email) {
         return res.status(400).json({
           status: "failed",
@@ -148,14 +147,14 @@ const generateOTPCode = () => {
   
       let user;
   
-      // Find the user based on the provided credentials
       if (email) {
+        // If an email is provided, find the user by email
         user = await User.findOne({ email });
-      } else if (username) {
+      } else {
+        // If username is provided, find the user by username
         user = await User.findOne({ username });
       }
   
-      // Check if a user with the provided credentials exists
       if (!user) {
         return res.status(404).json({
           status: "failed",
@@ -163,7 +162,6 @@ const generateOTPCode = () => {
         });
       }
   
-      // Validate the provided password
       const isPasswordValid = await bcrypt.compare(password, user.password);
   
       if (!isPasswordValid) {
@@ -173,7 +171,6 @@ const generateOTPCode = () => {
         });
       }
   
-      // Check if the user's email is verified
       if (!user.verifiedEmail) {
         return res.status(400).json({
           status: "failed",
@@ -185,8 +182,12 @@ const generateOTPCode = () => {
       user.activeStatus = "online";
       await user.save();
   
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SEC_KEY, {
+        expiresIn: "24h",
+      });
+  
       // Create a user details object with the desired fields
-      const userDetails = {
+      let userDetails = {
         _id: user._id,
         firstName: user.firstName,
         lastName: user.lastName,
@@ -205,14 +206,24 @@ const generateOTPCode = () => {
         interestedIn: user.interestedIn,
         lookingFor: user.lookingFor,
         bio: user.bio,
+        socialMedia: user.socialMedia,
+        pastRelationship: user.pastRelationship,
+        yourChoiceOfPartner: user.yourChoiceOfPartner,
+        redFlags: user.redFlags,
+        dealBreaker: user.dealBreaker,
+        dislikes: user.dislikes,
+        likes: user.likes,
+        expectations: user.expectations,
+        yourKindOfPartner: user.yourKindOfPartner,
+        yourIdealPartner: user.yourIdealPartner,
+        profilePhotos: user.profilePhotos,
+        timelinePhotos: user.timelinePhotos,
+        followers: user.followers,
+        following: user.following,
+        messages: user.messages,
         createdAt: user.createdAt,
         updatedAt: user.updatedAt,
       };
-  
-      // Create a JWT token for authentication
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SEC_KEY, {
-        expiresIn: "24h",
-      });
   
       return res.status(200).json({
         status: "success",
@@ -221,7 +232,7 @@ const generateOTPCode = () => {
         user: userDetails,
       });
     } catch (error) {
-      console.error(error);
+      console.log(error);
       return res.status(500).json({
         status: "failed",
         message: "Internal server error",
@@ -229,10 +240,190 @@ const generateOTPCode = () => {
     }
   };
   
+/////////////////////////// reques otp
+  const requestOTP = async (req, res) => {
+    const { email } = req.body;
+  
+    // Check if the email is provided
+    if (!email) {
+      return res.status(400).json({
+        status: "failed",
+        message: "Email cannot be blank",
+      });
+    }
+  
+    try {
+      // Check if the user with the provided email exists and is unverified
+      const existingUser = await User.findOne({ email, verifiedEmail: false });
+  
+      if (!existingUser) {
+        return res.status(400).json({
+          status: "failed",
+          message: "User with the provided email not found or already verified.",
+        });
+      }
+  
+      // Regenerate a new OTP for the existing unverified user
+      const otpCode = generateOTPCode();
+      const expirationTime = new Date(Date.now() + 10 * 60 * 1000);
+  
+      // Save the new OTP code to the database
+      const newOTPCode = new OTPCode({
+        userId: existingUser._id,
+        code: otpCode,
+        createdAt: Date.now(),
+        expiresAt: expirationTime,
+      });
+      await newOTPCode.save();
+  
+      // Resend the OTP to the user's email
+      const mailOptions = {
+        from: process.env.AUTH_EMAIL,
+        to: existingUser.email,
+        subject: "Verify Your Email",
+        html: `
+            <h1>Email Verification</h1>
+            <p><strong>${otpCode}</strong></p>
+            <p>Please enter the verification code in your account settings to verify your email.</p>
+          `,
+      };
+  
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+          return res.status(500).json({
+            status: "failed",
+            message: "An error occurred while resending the verification code.",
+          });
+        } else {
+          console.log("Email sent: " + info.response);
+          return res.status(200).json({
+            status: "success",
+            message:
+              "Verification code has been resent. Please check your email.",
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        status: "failed",
+        message: "An error occurred while resending the verification code.",
+      });
+    }
+  };
+  
+  ///////////////////verify otp
+  const verifyOTP = async (req, res) => {
+    // Extract the userId and the verification code from the request body
+    const { userId, verificationCode } = req.body;
+  
+    try {
+      const otpCodeRecord = await OTPCode.findOne({ userId });
+  
+      if (!otpCodeRecord) {
+        return res.status(400).json({
+          status: "failed",
+          message: "Invalid verification code.",
+        });
+      }
+  
+      if (otpCodeRecord.expiresAt < Date.now()) {
+        await OTPCode.deleteOne({ userId });
+        return res.status(400).json({
+          status: "failed",
+          message: "Verification code has expired. Please request a new one.",
+        });
+      }
+  
+      const isMatch = verificationCode === otpCodeRecord.code;
+  
+      if (!isMatch) {
+        return res.status(400).json({
+          status: "failed",
+          message: "Invalid verification code.",
+        });
+      }
+  
+      // Mark the user as verified (you can add this field to your User model)
+      await User.updateOne({ _id: userId }, { verifiedEmail: true });
+  
+      // Retrieve the user data after successful account verification
+      const user = await User.findOne({ _id: userId });
+  
+      // Generate JWT token
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SEC_KEY, {
+        expiresIn: "24h",
+      });
+  
+      return res.status(200).json({
+        status: "success",
+        message: "Account verification successful.",
+        token,
+        user,
+      });
+    } catch (error) {
+      console.error("Error while verifying the account:", error);
+      return res.status(500).json({
+        status: "failed",
+        message: "An error occurred while verifying the account.",
+      });
+    }
+  };
+  
+  /////// CHNGE PASSWORD
+  const changePassword = async (req, res) => {
+    const { currentPassword, newPassword } = req.body;
+    const userId = req.user.userId; // Make sure 'userId' matches the property name in req.user
+  
+    try {
+      const user = await User.findById(userId);
+  
+      if (!user) {
+        return res.status(404).json({
+          status: 'failed',
+          message: 'User not found. Please log in again.',
+        });
+      }
+  
+      // Verify the current password
+      const isPasswordValid = await bcrypt.compare(currentPassword, user.password);
+  
+      if (!isPasswordValid) {
+        return res.status(400).json({
+          status: 'failed',
+          message: 'Invalid current password. Please enter the correct password.',
+        });
+      }
+  
+      // Hash the new password
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(newPassword, salt);
+  
+      // Update the user's password
+      user.password = hashedPassword;
+      await user.save();
+  
+      return res.status(200).json({
+        status: 'success',
+        message: 'Password changed successfully.',
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        status: 'failed',
+        message: 'An error occurred while processing your request.',
+      });
+    }
+  };
+  
   
   const authController = {
     loginUser,
-    registerUser
+    registerUser,
+    requestOTP,
+    verifyOTP,
+    changePassword,
   };
   
   module.exports = authController;
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const router = express.Router();
 //const validateFileds = require('../validator/validate');
-const {registerUser, loginUser} = require('../controllers/authController');
+const {registerUser, loginUser, requestOTP, verifyOTP, changePassword} = require('../controllers/authController');
+const {verifyToken, verifyUser, verifyAdmin, verifyModerator} = require('../middleWare/authMiddleWare')
 
 router.get('/', (req, res)=>{
     res.send(' running here');
-}
-)
+})
 
-// User registration route
+// User registration route registerUser
 router.post('/register', registerUser);
+
+/////// LOGIN-ROUTE loginUser
 router.post('/login', loginUser);
+
+//////REQUEST OTP requestOTP
+router.post('/requestotp', requestOTP);
+
+////////VERIFY OTP verifyOTP
+router.post('/verifyotp', verifyOTP);
+
+
+///// CHANGE PASSWORD changePassword
+router.put('/changePassword', verifyToken, verifyUser, changePassword);
+
+
+
+
+
 
 
 
